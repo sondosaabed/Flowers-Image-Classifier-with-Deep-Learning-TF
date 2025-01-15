875346b946e0ff2e4fd4937de7182babe9e307ca
Sondos Aabed <sondosahmedabed@gmail.com>
2025-01-15 22:37:01 +0200

    add utill of image processing

--- cmd-app/utils.py
+++ cmd-app/utils.py
@@ -1,1 +1,17 @@
+import numpy as np
+from PIL import Image
+import tensorflow as tf
 
+def process_image(image_path):
+    """
+    Args:
+        - image_path
+    Returns:
+        - Preprocessed image
+    """
+    image = Image.open(image_path)
+    image = np.asarray(image)
+    image = tf.image.resize(image, (224, 224))
+    image = tf.cast(image, tf.float32)
+    image /= 255
+    return image
