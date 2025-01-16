2a2f4b349f404b6ade75b2316cc815e526066ba4
Sondos Aabed <sondosahmedabed@gmail.com>
2025-01-16 12:15:56 +0200

    Add get args from parser

--- cmd-app/scripts/parser.py
+++ cmd-app/scripts/parser.py
@@ -12,3 +12,8 @@ parser.add_argument("saved_keras_model", help="path to the saved Keras model", t
 parser.add_argument("-k", "--top_k", default=3, help ="top k class probabilities", type=int)
 parser.add_argument("-n", "--category_names", default="./label_map.json", help="path to a JSON file mapping labels to the actual flower names", type=str)
 
+def get_args():
+    """
+    Returns the parser's args
+    """
+    return parser.parse_args()
