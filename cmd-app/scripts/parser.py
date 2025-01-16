682a9bee88f70dfb2f047b6ad024c9e90d720d62
Sondos Aabed <sondosahmedabed@gmail.com>
2025-01-16 13:00:35 +0200

    Add the assets folder and update paths in the app

--- cmd-app/scripts/parser.py
+++ cmd-app/scripts/parser.py
@@ -5,11 +5,11 @@ parser = argparse.ArgumentParser(description="A cmd application helps to identif
 
 # Positional arguments 
 parser.add_argument("image_path", help="path to the input image folder", type=str)
-parser.add_argument("saved_keras_model", help="path to the saved Keras model", type=str)
+parser.add_argument("saved_keras_model", default="cmd-app/assets/best_model.h5", help="path to the saved Keras model", type=str)
 
 # Optional arguments 
 parser.add_argument("-k", "--top_k", default=3, help ="top k class probabilities", type=int)
-parser.add_argument("-n", "--category_names", default="./label_map.json", help="path to a JSON file mapping labels to the actual flower names", type=str)
+parser.add_argument("-n", "--category_names", default="cmd-app/assets/label_map.json", help="path to a JSON file mapping labels to the actual flower names", type=str)
 
 def get_args():
     """
